280579f2635d5788fd439852e4a294a5898f5161
saikv <saikrishnagrc35@gmail.com>
2025-11-05 00:03:40 +0530

    m

---
+++ data2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "dhoni" "sachin")
+
+echo "first movie ; ${MOVIES[0]}"
+echo "second movie ; ${MOVIES[1]}"
+echo "third movie ; ${MOVIES[2]}"
+
+echo "all movies ; ${MOVIES[0]}"
