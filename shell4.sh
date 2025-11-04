7a4359f01be8510ca5022486026730c36e8df8af
saikv <saikrishnagrc35@gmail.com>
2025-11-04 14:42:14 +0530

    m

---
+++ shell4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+USERNAME=dhoni
+PASSWORD=sai
+
+echo "please enter your username="
+read USERNAME
+echo "username entered=$USERNAME"
+
+echo "please enter password"
+read PASSWORD
+echo "password entered=$PASSWORD"
