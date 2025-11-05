5de6be28e878523927b234c629d03ef812264376
saikv <saikrishnagrc35@gmail.com>
2025-11-05 14:00:08 +0530

    m

---
+++ sql6.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+  echo "ERROR: You must have sudo access to run this script"
+  exit 1
+else 
+  echo "Proceeding further..."
+fi
+
+apt update -y
+
+# Install MySQL
+apt install mysql-server -y
+if [ $? -ne 0 ]
+then 
+  echo "Installing MySQL... failure"
+  exit 1
+else
+  echo "Installing MySQL... success"
+fi
+
+# Install NGINX
+apt install nginx -y
+if [ $? -ne 0 ]
+then 
+  echo "Installing NGINX... failure"
+  exit 1
+else
+  echo "Installing NGINX... success"
+fi
