98ff6af2d359ccffbd83bdcbd69300a65d23289c
saikv <saikrishnagrc35@gmail.com>
2025-11-05 12:28:26 +0530

    m

---
+++ sql2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+  echo "ERROR: You must have sudo access to run this script"
+  exit 1
+fi
+
+apt update -y
+apt install mysqlll
+apt install nginx -y
